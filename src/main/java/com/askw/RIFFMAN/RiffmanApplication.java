a52eb94f496172e1ab827ad83480362106eef506
asakshun <asakshun312@gmail.com>
2024-11-05 23:36:49 +0900

    first-commit

---
+++ src/main/java/com/askw/RIFFMAN/RiffmanApplication.java
@@ -0,0 +1,13 @@
+package com.askw.RIFFMAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiffmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/askw/RIFFMAN/RiffmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askw.RIFFMAN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiffmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
